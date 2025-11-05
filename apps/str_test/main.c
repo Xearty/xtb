ca60f25fd4b34dd34ba50881020c2bc0eef3b1e6
Martin Ivanov <xeartybg@gmail.com>
2025-11-05 16:32:42 +0200

    refactor(str_test): Use shorthands for strings everywhere

--- apps/str_test/main.c
+++ apps/str_test/main.c
@@ -18,42 +18,42 @@ int main(int argc, char **argv)
     XTB_Arena *arena = xtb_arena_new(XTB_Kilobytes(4));
     XTB_Allocator allocator = xtb_arena_allocator(arena);
 
-    XTB_String8 string = xtb_str8_lit("hello");
+    String8 string = str8_lit("hello");
     puts(string.str);
     printf("length = %zu\n", string.len);
 
-    XTB_String8 dyn_string = xtb_str8_lit_copy(allocator, "hello there");
+    String8 dyn_string = str8_lit_copy(allocator, "hello there");
     printf("dyn_string = %s, length = %zu\n", dyn_string.str, dyn_string.len);
 
-    XTB_String8_List str_list = {0};
-    XTB_String8_List_Node node1 = { xtb_str8_lit("hello ") };
-    XTB_String8_List_Node node2 = { xtb_str8_lit("world") };
+    String8_List str_list = {0};
+    String8_List_Node node1 = { str8_lit("hello ") };
+    String8_List_Node node2 = { str8_lit("world") };
 
     SLLQueuePush(str_list.head, str_list.tail, &node1);
     SLLQueuePush(str_list.head, str_list.tail, &node2);
 
-    XTB_String8 concatenated = xtb_str8_list_join(allocator, str_list);
+    String8 concatenated = str8_list_join(allocator, str_list);
     puts(concatenated.str);
 
-    XTB_String8 test2 = xtb_str8_lit("Nqkakuv dulug string");
-    XTB_String8 sub_test2 = xtb_str8_substr_copy(allocator, test2, 10, 5);
+    String8 test2 = str8_lit("Nqkakuv dulug string");
+    String8 sub_test2 = str8_substr_copy(allocator, test2, 10, 5);
     printf("str = \"%s\", len = %zu\n", sub_test2.str, sub_test2.len);
 
     XTB_Temp_Arena temp = xtb_scratch_begin(NULL, 0);
     XTB_Allocator temp_allocator = xtb_arena_allocator(temp.arena);
 
-    XTB_String8_List list = {0};
-    xtb_str8_list_push(temp_allocator, &list, xtb_str8_lit("a"));
-    xtb_str8_list_push(temp_allocator, &list, xtb_str8_lit("b"));
-    xtb_str8_list_push(temp_allocator, &list, xtb_str8_lit("c"));
-    xtb_str8_list_push(temp_allocator, &list, xtb_str8_lit("d"));
-    xtb_str8_list_push(temp_allocator, &list, xtb_str8_lit("e"));
+    String8_List list = {0};
+    str8_list_push(temp_allocator, &list, str8_lit("a"));
+    str8_list_push(temp_allocator, &list, str8_lit("b"));
+    str8_list_push(temp_allocator, &list, str8_lit("c"));
+    str8_list_push(temp_allocator, &list, str8_lit("d"));
+    str8_list_push(temp_allocator, &list, str8_lit("e"));
 
-    XTB_String8 sep = xtb_str8_lit(".");
-    XTB_String8 joined = xtb_str8_list_join_str_sep(allocator, list, sep);
+    String8 sep = str8_lit(".");
+    String8 joined = str8_list_join_str_sep(allocator, list, sep);
     puts(joined.str);
 
-    if (xtb_str8_eq_lit(joined, "a.b.c.d.e"))
+    if (str8_eq_lit(joined, "a.b.c.d.e"))
     {
         puts("The strings are equal");
     }
@@ -62,68 +62,68 @@ int main(int argc, char **argv)
         puts("The strings are different");
     }
 
-    XTB_String8 test3 = xtb_str8_lit("Hello world");
-    test3 = xtb_str8_trunc_left(test3, 3);
-    test3 = xtb_str8_trunc_right(test3, 4);
-    test3 = xtb_str8_copy(temp_allocator, test3);
+    String8 test3 = str8_lit("Hello world");
+    test3 = str8_trunc_left(test3, 3);
+    test3 = str8_trunc_right(test3, 4);
+    test3 = str8_copy(temp_allocator, test3);
     puts(test3.str);
 
-    XTB_String8 test4 = xtb_str8_lit("\t \n  First  \n \t  \t\t \r Second\n \t\t \r\n");
-    XTB_String8 test5 = test4;
-    test5 = xtb_str8_copy(temp_allocator, xtb_str8_trim_left(test4));
+    String8 test4 = str8_lit("\t \n  First  \n \t  \t\t \r Second\n \t\t \r\n");
+    String8 test5 = test4;
+    test5 = str8_copy(temp_allocator, str8_trim_left(test4));
     printf("Left trimmed: \"%s\"\n", test5.str);
 
-    test5 = xtb_str8_copy(temp_allocator, xtb_str8_trim_right(test4));
+    test5 = str8_copy(temp_allocator, str8_trim_right(test4));
     printf("Right trimmed: \"%s\"\n", test5.str);
 
-    test5 = xtb_str8_copy(temp_allocator, xtb_str8_trim(test4));
+    test5 = str8_copy(temp_allocator, str8_trim(test4));
     printf("Trimmed both ways: \"%s\"\n", test5.str);
 
-    XTB_String8 test6 = xtb_str8_trim_copy(temp_allocator, test4);
+    String8 test6 = str8_trim_copy(temp_allocator, test4);
     printf("Trimmed both ways copy: \"%s\"\n", test6.str);
 
-    XTB_String8 char_split_str = xtb_str8_lit("Very long string that contains multiple tokens");
-    XTB_String8_List char_split_list = xtb_str8_split_by_char(temp_allocator, char_split_str, ' ');
-    XTB_String8 char_joined = xtb_str8_list_join_str_sep(temp_allocator, char_split_list, xtb_str8_lit("<sep>"));
+    String8 char_split_str = str8_lit("Very long string that contains multiple tokens");
+    String8_List char_split_list = str8_split_by_char(temp_allocator, char_split_str, ' ');
+    String8 char_joined = str8_list_join_str_sep(temp_allocator, char_split_list, str8_lit("<sep>"));
     puts(char_joined.str);
 
-    XTB_String8 str_split_str = xtb_str8_lit("Very long  string that contains  multiple tokens");
-    XTB_String8_List str_split_list = xtb_str8_split_by_str(temp_allocator, str_split_str, xtb_str8_lit("  "));
-    XTB_String8 str_joined = xtb_str8_list_join_str_sep(temp_allocator, str_split_list, xtb_str8_lit("---"));
+    String8 str_split_str = str8_lit("Very long  string that contains  multiple tokens");
+    String8_List str_split_list = str8_split_by_str(temp_allocator, str_split_str, str8_lit("  "));
+    String8 str_joined = str8_list_join_str_sep(temp_allocator, str_split_list, str8_lit("---"));
     puts(str_joined.str);
 
-    XTB_String8 space_split_str = xtb_str8_lit("\t\n\r\r\n Very \t long  \rstring\r\rthat contains \r \nmultiple \n\r\r\n\ttokens\t\n\t");
-    XTB_String8_List space_split_list = xtb_str8_split_by_whitespace(temp_allocator, space_split_str);
-    XTB_String8 space_joined = xtb_str8_list_join_char_sep(temp_allocator, space_split_list, '-');
+    String8 space_split_str = str8_lit("\t\n\r\r\n Very \t long  \rstring\r\rthat contains \r \nmultiple \n\r\r\n\ttokens\t\n\t");
+    String8_List space_split_list = str8_split_by_whitespace(temp_allocator, space_split_str);
+    String8 space_joined = str8_list_join_char_sep(temp_allocator, space_split_list, '-');
     puts(space_joined.str);
 
-    XTB_String8 concatenated2 = xtb_str8_list_join(temp_allocator, space_split_list);
-    xtb_str8_debug(concatenated2);
+    String8 concatenated2 = str8_list_join(temp_allocator, space_split_list);
+    str8_debug(concatenated2);
 
     xtb_scratch_end(temp);
 
-    XTB_String8 formatted = xtb_str8_format(allocator, "The answer is %d", 42);
-    xtb_str8_debug(formatted);
+    String8 formatted = str8_format(allocator, "The answer is %d", 42);
+    str8_debug(formatted);
 
-    XTB_String8 head_tail = xtb_str8_lit("head<>tail");
-    XTB_String8 head = xtb_str8_head(head_tail, 4);
-    XTB_String8 tail = xtb_str8_tail(head_tail, 4);
-    xtb_str8_debug(head_tail);
-    xtb_str8_debug(head);
-    xtb_str8_debug(tail);
+    String8 head_tail = str8_lit("head<>tail");
+    String8 head = str8_head(head_tail, 4);
+    String8 tail = str8_tail(head_tail, 4);
+    str8_debug(head_tail);
+    str8_debug(head);
+    str8_debug(tail);
 
-    if (!xtb_str8_starts_with_lit(head_tail, "head"))
+    if (!str8_starts_with_lit(head_tail, "head"))
     {
         XTB_ASSERT(false);
     }
 
-    if (!xtb_str8_ends_with_lit(head_tail, "tail"))
+    if (!str8_ends_with_lit(head_tail, "tail"))
     {
         XTB_ASSERT(false);
     }
 
-    XTB_String8 concat = xtb_str8_concat_lit(allocator, xtb_str8_lit("hello "), "world");
-    xtb_str8_debug(concat);
+    String8 concat = str8_concat_lit(allocator, str8_lit("hello "), "world");
+    str8_debug(concat);
 
     String8 str = str8_lit("hello");
     str = str8_concat(allocator, str, str8_lit(" world"));
