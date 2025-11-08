0f2083b9e0915181156e001ea31dcfcf27d9af0a
Martin Ivanov <xeartybg@gmail.com>
2025-11-08 05:15:46 +0200

    fix(xtb_ogl): C linkage

--- libs/xtb_ogl/ogl.h
+++ libs/xtb_ogl/ogl.h
@@ -4,6 +4,8 @@
 #include <xtb_core/str.h>
 #include <glad/glad.h>
 
+XTB_C_LINKAGE_BEGIN
+
 typedef unsigned int ShaderProgram;
 typedef unsigned int Shader;
 
@@ -12,5 +14,7 @@ Shader load_vertex_shader_from_file(const char *ns, XTB_String8 filepath);
 Shader load_fragment_shader_from_file(const char *ns, XTB_String8 filepath);
 ShaderProgram create_shader_program_from_descriptors(const char *ns, Shader vs_id, Shader fs_id);
 ShaderProgram load_shader_program_from_files(const char *ns, XTB_String8 vertex_filepath, XTB_String8 fragment_filepath);
+
+XTB_C_LINKAGE_END
 
 #endif // _XTB_OGL_H_
