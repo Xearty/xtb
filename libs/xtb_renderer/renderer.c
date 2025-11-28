19cd9d854ced0d3256445c03845b25fa8659ce18
Martin Ivanov <xeartybg@gmail.com>
2025-11-28 02:48:07 +0200

    refactor(renderer): Split code into multiple files

--- libs/xtb_renderer/renderer.c
+++ libs/xtb_renderer/renderer.c
@@ -1,132 +1,17 @@
 #include "renderer.h"
 
-#include "types.h"
 #include <xtb_core/arena.h>
 #include <xtb_core/thread_context.h>
 #include "generated/baked_shaders_generated.h"
 
 #include "material.c"
 #include "opengl_material.c"
+#include "polyline.c"
+#include "bezier.c"
 
 /****************************************************************
  * Internal
 ****************************************************************/
-static Vec2Array compute_bezier_points(Allocator *allocator, vec2 *points, size_t count, i32 samples)
-{
-    Assert(samples > 1);
-    Assert(count > 0);
-
-    TempArena scratch = scratch_begin_conflict(allocator);
-
-    Vec2Array polypoints = make_array(allocator);
-    array_reserve(&polypoints, samples);
-
-    Vec2Array intermediate = make_array(&scratch.arena->allocator);
-
-    for (i32 sample_iter = 0; sample_iter < samples; ++sample_iter)
-    {
-        f32 t = unlerp(0, samples - 1, sample_iter);
-
-        i32 icount = count;
-        array_assign(&intermediate, points, count);
-
-        while (icount > 1)
-        {
-            for (i32 i = 0; i < icount - 1; ++i)
-            {
-                intermediate.data[i] = lerp2(intermediate.data[i], intermediate.data[i + 1], t);
-            }
-
-            icount -= 1;
-        }
-
-        array_push(&polypoints, intermediate.data[0]);
-    }
-
-    scratch_end(scratch);
-    return polypoints;
-}
-
-static Vec2Array compute_bezier_spline_points(Allocator *allocator, vec2 *points, i32 count, i32 bezier_deg, i32 samples)
-{
-    Vec2Array polypoints = make_array(allocator);
-
-    for (i32 i = 0; i < count - bezier_deg; i += bezier_deg)
-    {
-        Vec2Array curr_polypoints = compute_bezier_points(allocator, points + i, bezier_deg + 1, samples);
-
-        if (i != 0)
-        {
-            array_append(&polypoints, curr_polypoints.data + 1, curr_polypoints.count - 1);
-        }
-        else
-        {
-            array_append(&polypoints, curr_polypoints.data, curr_polypoints.count);
-        }
-    }
-
-    return polypoints;
-}
-
-static void renderer_setup_polyline_data(Renderer *renderer, PolylineRenderData *out_data)
-{
-    PolylineRenderData *rdata = &renderer->polyline_render_data;
-
-    glGenVertexArrays(1, &rdata->vao);
-    glBindVertexArray(rdata->vao);
-
-    glGenBuffers(1, &rdata->per_vertex_vbo);
-    glGenBuffers(1, &rdata->instanced_vbo);
-    glGenBuffers(1, &rdata->instanced_ebo);
-
-    i32 per_vertex_stride = sizeof(PolylinePerVertexData);
-    glBindBuffer(GL_ARRAY_BUFFER, rdata->per_vertex_vbo);
-
-    glVertexAttribPointer(0, 1, GL_FLOAT, GL_FALSE, per_vertex_stride, (void *)offsetof(PolylinePerVertexData, t));
-    glEnableVertexAttribArray(0);
-
-    glVertexAttribPointer(1, 1, GL_FLOAT, GL_FALSE, per_vertex_stride, (void *)offsetof(PolylinePerVertexData, side));
-    glEnableVertexAttribArray(1);
-
-    PolylinePerVertexData per_vertex_data[] = {
-        {0.0f, 1.0f},
-        {1.0f, 1.0f},
-        {1.0f, -1.0f},
-        {0.0f, -1.0f}};
-
-    glBufferData(GL_ARRAY_BUFFER, sizeof(per_vertex_data), per_vertex_data, GL_STATIC_DRAW);
-
-    u32 indices[] = {
-        0, 1, 2, //
-        2, 3, 0  //
-    };
-
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, rdata->instanced_ebo);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
-
-    i32 instanced_stride = sizeof(PolylineInstanceData);
-    glBindBuffer(GL_ARRAY_BUFFER, rdata->instanced_vbo);
-    glBufferData(GL_ARRAY_BUFFER, POLYLINE_INSTANCED_MAX_LINES * instanced_stride, NULL, GL_DYNAMIC_DRAW);
-
-    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, instanced_stride, (void *)offsetof(PolylineInstanceData, prev));
-    glEnableVertexAttribArray(2);
-    glVertexAttribDivisor(2, 1);
-
-    glVertexAttribPointer(3, 2, GL_FLOAT, GL_FALSE, instanced_stride, (void *)offsetof(PolylineInstanceData, current_start));
-    glEnableVertexAttribArray(3);
-    glVertexAttribDivisor(3, 1);
-
-    glVertexAttribPointer(4, 2, GL_FLOAT, GL_FALSE, instanced_stride, (void *)offsetof(PolylineInstanceData, current_end));
-    glEnableVertexAttribArray(4);
-    glVertexAttribDivisor(4, 1);
-
-    glVertexAttribPointer(5, 2, GL_FLOAT, GL_FALSE, instanced_stride, (void *)offsetof(PolylineInstanceData, next));
-    glEnableVertexAttribArray(5);
-    glVertexAttribDivisor(5, 1);
-
-    glBindVertexArray(0);
-}
-
 static inline GpuMesh* ensure_quad_mesh(Renderer *renderer)
 {
     if (renderer->mesh_cache.quad) return renderer->mesh_cache.quad;
@@ -237,7 +122,7 @@ void renderer_init(Renderer *renderer, f32 width, f32 height)
 
     init_standard_vao(renderer);
 
-    renderer_setup_polyline_data(renderer, &renderer->polyline_render_data);
+    setup_polyline_render_data(renderer, &renderer->polyline_render_data);
 
     camera_init(&renderer->camera2d);
     camera_init(&renderer->camera3d);
@@ -265,6 +150,9 @@ void renderer_cameras_recreate_projections(Renderer *renderer, f32 width, f32 he
     camera_set_projection(&renderer->camera3d, perspective_proj);
 }
 
+/****************************************************************
+ * Rendering Functions
+****************************************************************/
 static void render_mesh_geometry(Renderer *renderer, const GpuMesh *mesh)
 {
     u32 vao = renderer->standard_vao;
@@ -282,21 +170,18 @@ static void render_mesh_geometry(Renderer *renderer, const GpuMesh *mesh)
     }
 }
 
-/****************************************************************
- * Rendering Functions
-****************************************************************/
-static void material_set_mvp(Renderer *renderer, Material *material, mat4 model)
-{
-    material_set_mat4(material, "model", model);
-    material_set_mat4(material, "view", renderer->camera3d.view);
-    material_set_mat4(material, "projection", renderer->camera3d.projection);
-}
-
 // Implies mvp vertex shader
 static void render_mesh(Renderer *renderer, GpuMesh *mesh, Material *material)
 {
     material_apply(material);
     render_mesh_geometry(renderer, mesh);
+}
+
+static void material_set_mvp(Renderer *renderer, Material *material, mat4 model)
+{
+    material_set_mat4(material, "model", model);
+    material_set_mat4(material, "view", renderer->camera3d.view);
+    material_set_mat4(material, "projection", renderer->camera3d.projection);
 }
 
 void render_quad(Renderer *renderer, vec4 color, mat4 transform)
@@ -317,7 +202,7 @@ void render_cube(Renderer *renderer, vec4 color, mat4 transform)
     TempArena scratch = scratch_begin_no_conflicts();
 
     Material material = material_copy(&scratch.arena->allocator, &renderer->default_solid_color_material);
-    material_set_vec4(&material, "color", color); // TODO: Default is not taken into account
+    material_set_vec4(&material, "color", color);
     material_set_mvp(renderer, &material, transform);
 
     render_mesh(renderer, ensure_cube_mesh(renderer), &material);
@@ -325,59 +210,3 @@ void render_cube(Renderer *renderer, vec4 color, mat4 transform)
     scratch_end(scratch);
 }
 
-void render_polyline_custom(Renderer *renderer, vec2 *points, i32 count, f32 thickness, vec4 color, bool looped)
-{
-    const PolylineRenderData *rdata = &renderer->polyline_render_data;
-
-    ShaderProgram program = renderer->shaders.polyline;
-    glUseProgram(program);
-    glUniform1f(glGetUniformLocation(program, "uThickness"), thickness);
-    glUniformMatrix4fv(glGetUniformLocation(program, "uProjection"), 1, GL_FALSE, &renderer->camera2d.projection.m00);
-    glUniform4f(glGetUniformLocation(program, "color"), color.r, color.g, color.b, color.a);
-
-    glBindVertexArray(rdata->vao);
-
-    for (i32 batch_start = 0; batch_start < count - 1; batch_start += POLYLINE_INSTANCED_MAX_LINES)
-    {
-        PolylineInstanceData instanced_data[POLYLINE_INSTANCED_MAX_LINES] = {};
-
-        i32 batch_end = Min(batch_start + POLYLINE_INSTANCED_MAX_LINES, count - 1);
-
-        for (i32 i = batch_start; i < batch_end; ++i)
-        {
-            PolylineInstanceData *inst_data = &instanced_data[i - batch_start];
-
-            if (looped)
-            {
-                inst_data->prev = points[repeati32(i - 1, count - 1)];
-                inst_data->current_start = points[i];
-                inst_data->current_end = points[repeati32(i + 1, count - 1)];
-                inst_data->next = points[repeati32(i + 2, count - 1)];
-            }
-            else
-            {
-                inst_data->prev = points[Max(0, i - 1)];
-                inst_data->current_start = points[i];
-                inst_data->current_end = points[i + 1];
-                inst_data->next = points[Min(i + 2, count - 1)];
-            }
-        }
-
-        i32 instances_in_batch = batch_end - batch_start;
-
-        glBindBuffer(GL_ARRAY_BUFFER, rdata->instanced_vbo);
-        glBufferSubData(GL_ARRAY_BUFFER, 0, instances_in_batch * sizeof(PolylineInstanceData), instanced_data);
-
-        glDrawElementsInstanced(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0, instances_in_batch);
-    }
-
-    glBindVertexArray(0);
-}
-
-void render_bezier_spline_custom(Renderer *renderer, vec2 *points, i32 count, i32 bezier_deg, i32 samples, f32 thickness, vec4 color, bool looped)
-{
-    TempArena scratch = scratch_begin_no_conflicts();
-    Vec2Array polypoints = compute_bezier_spline_points(&scratch.arena->allocator, points, count, bezier_deg, samples);
-    render_polyline_custom(renderer, polypoints.data, polypoints.count, thickness, color, looped);
-    scratch_end(scratch);
-}
