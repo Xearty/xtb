1cbf4f3d8758ba9b23ca56037b8850268f8e7650
Martin Ivanov <xeartybg@gmail.com>
2026-07-31 16:40:47 +0300

    build: Add reproducible BetterC tooling

---
+++ xtbd/flake.nix
@@ -0,0 +1,95 @@
+{
+  description = "xtbd BetterC development environment";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+  outputs = {
+    self,
+    nixpkgs,
+  }: let
+    supportedSystems = [
+      "x86_64-linux"
+      "aarch64-linux"
+      "aarch64-darwin"
+    ];
+    forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+  in {
+    formatter = forAllSystems (system: nixpkgs.legacyPackages.${system}.alejandra);
+
+    packages = forAllSystems (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+    in rec {
+      default = xtbd;
+      xtbd = pkgs.stdenv.mkDerivation {
+        pname = "xtbd";
+        version = "0.1.0";
+        src = self;
+        nativeBuildInputs = [pkgs.ldc];
+        dontConfigure = true;
+        buildPhase = ''
+          runHook preBuild
+          mkdir -p build
+          ldc2 -betterC -boundscheck=on -wi -de -I=source \
+            -lib $(find source -name '*.d' -print | sort) \
+            -of=build/libxtbd.a
+          runHook postBuild
+        '';
+        installPhase = ''
+          runHook preInstall
+          mkdir -p $out/lib $out/include
+          cp build/libxtbd.a $out/lib/
+          cp -R source/xtb $out/include/
+          runHook postInstall
+        '';
+      };
+    });
+
+    checks = forAllSystems (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      package = self.packages.${system}.xtbd;
+      tests = pkgs.stdenv.mkDerivation {
+        pname = "xtbd-tests";
+        version = "0.1.0";
+        src = self;
+        nativeBuildInputs = [pkgs.ldc];
+        dontConfigure = true;
+        buildPhase = ''
+          runHook preBuild
+          ldc2 -betterC -unittest -boundscheck=on -wi -de -I=source \
+            $(find source -name '*.d' -print | sort) tests/core_tests.d \
+            -of=core-tests
+          ./core-tests
+          runHook postBuild
+        '';
+        installPhase = ''
+          mkdir -p $out
+          touch $out/passed
+        '';
+      };
+    });
+
+    devShells = forAllSystems (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      default = pkgs.mkShell {
+        name = "xtbd";
+        strictDeps = true;
+
+        packages = with pkgs; [
+          ldc
+          dub
+          dscanner
+          just
+          pkg-config
+          clang-tools
+          lldb
+        ];
+
+        shellHook = ''
+          echo "xtbd BetterC shell: $(ldc2 --version | head -n 1)"
+        '';
+      };
+    });
+  };
+}
