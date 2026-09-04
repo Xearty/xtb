0b850ce1896c118d8185ec11bee0b6717d171ea1
Martin Ivanov <xeartybg@gmail.com>
2026-09-05 01:43:35 +0300

    style(core): migrate data_struct module

--- source/core/xtb/data_struct.d
+++ source/core/xtb/data_struct.d
@@ -1,8 +1,5 @@
 module xtb.data_struct;
 
-version (unittest) import xtb.lifetime : move;
-version (unittest) import xtb.types : i32;
-
 /// Generates a consuming memberwise constructor that requires every field.
 ///
 /// Fields with declaration-site defaults remain required. The constructor
@@ -11,53 +8,72 @@ version (unittest) import xtb.types : i32;
 ///
 /// `Type.init` remains available because it is an intrinsic property of every
 /// D type. Do not add constructors that bypass the complete-field requirement.
-mixin template DataStruct() {
-    static assert(typeof(this).tupleof.length != 0,
-        "DataStruct requires at least one instance field");
+mixin template DataStruct()
+{
+    static assert(
+        typeof(this).tupleof.length != 0,
+        "DataStruct requires at least one instance field",
+    );
 
-    mixin((() {
-        alias T = typeof(this);
-        auto move_name = "data_struct_move";
-        bool move_name_in_use = true;
+    mixin(
+        (()
+        {
+            alias T = typeof(this);
+            auto move_name = "data_struct_move";
+            bool move_name_in_use = true;
 
-        while (move_name_in_use) {
-            move_name_in_use = false;
+            while (move_name_in_use)
+            {
+                move_name_in_use = false;
 
-            static foreach (index; 0 .. T.tupleof.length) {
-                if (move_name == __traits(identifier, T.tupleof[index])) {
-                    move_name_in_use = true;
+                static foreach (index; 0 .. T.tupleof.length)
+                {
+                    if (move_name == __traits(identifier, T.tupleof[index]))
+                    {
+                        move_name_in_use = true;
+                    }
+                }
+
+                if (move_name_in_use)
+                {
+                    move_name ~= "_field";
                 }
             }
 
-            if (move_name_in_use) {
-                move_name ~= "_field";
-            }
-        }
+            auto declaration = "@disable this();\n\nthis(\n";
 
-        auto declaration = "@disable this();\n\nthis(\n";
+            static foreach (index; 0 .. T.tupleof.length)
+            {{
+                enum field_name = __traits(identifier, T.tupleof[index]);
+                declaration ~= "    typeof(this." ~ field_name ~ ") " ~ field_name ~ ",\n";
+            }}
 
-        static foreach (index; 0 .. T.tupleof.length) {{
-            enum field_name = __traits(identifier, T.tupleof[index]);
-            declaration ~= "    typeof(this." ~ field_name ~ ") " ~
-                field_name ~ ",\n";
-        }}
+            declaration ~= ") nothrow @nogc @trusted\n{\n";
+            declaration ~= "    import xtb.lifetime : " ~ move_name ~ " = move;\n\n";
 
-        declaration ~= ") nothrow @nogc @trusted {\n";
-        declaration ~= "    import xtb.lifetime : " ~ move_name ~ " = move;\n\n";
+            static foreach (index; 0 .. T.tupleof.length)
+            {{
+                enum field_name = __traits(identifier, T.tupleof[index]);
+                declaration ~= "    this." ~ field_name ~ " = " ~ move_name
+                    ~ "(" ~ field_name ~ ");\n";
+            }}
 
-        static foreach (index; 0 .. T.tupleof.length) {{
-            enum field_name = __traits(identifier, T.tupleof[index]);
-            declaration ~= "    this." ~ field_name ~ " = " ~ move_name ~ "(" ~
-                field_name ~ ");\n";
-        }}
-
-        declaration ~= "}\n";
-        return declaration;
-    })());
+            declaration ~= "}\n";
+            return declaration;
+        })(),
+    );
 }
 
-unittest {
-    struct Config {
+version (unittest)
+{
+    import xtb.lifetime;
+    import xtb.types;
+}
+
+unittest
+{
+    struct Config
+    {
         i32 width;
         i32 height;
 
@@ -69,12 +85,17 @@ unittest {
     assert(config.width == 800);
     assert(config.height == 600);
     static assert(!__traits(compiles, Config(width: 800)));
-    static assert(!__traits(compiles, () { Config value; }));
+    static assert(!__traits(compiles, ()
+    {
+        Config value;
+    }));
     static assert(__traits(compiles, Config.init));
 }
 
-unittest {
-    struct Defaults {
+unittest
+{
+    struct Defaults
+    {
         i32 value = 10;
 
         mixin DataStruct;
@@ -86,15 +107,18 @@ unittest {
     static assert(!__traits(compiles, Defaults()));
 }
 
-unittest {
-    struct Owner {
+unittest
+{
+    struct Owner
+    {
         i32 value;
 
         @disable this(this);
         @disable ref Owner opAssign(Owner source) return;
     }
 
-    struct OwnerConfig {
+    struct OwnerConfig
+    {
         Owner owner;
 
         mixin DataStruct;
@@ -107,8 +131,10 @@ unittest {
     assert(owner == Owner.init);
 }
 
-unittest {
-    struct MoveName {
+unittest
+{
+    struct MoveName
+    {
         i32 data_struct_move;
 
         mixin DataStruct;
