c3908f8f0c7ed547720826deb5a971903e56c88d
Martin Ivanov <xeartybg@gmail.com>
2026-09-04 13:52:10 +0300

    style(core): migrate metadata module

--- source/core/xtb/metadata.d
+++ source/core/xtb/metadata.d
@@ -1,28 +1,50 @@
 module xtb.metadata;
 
-enum versionMajor = 0;
-enum versionMinor = 1;
-enum versionPatch = 0;
-enum versionString = "0.1.0";
+enum version_major = 0;
+enum version_minor = 1;
+enum version_patch = 0;
+enum version_string = version_major.stringof
+    ~ "."
+    ~ version_minor.stringof
+    ~ "."
+    ~ version_patch.stringof;
 
 version (linux)
-    enum operatingSystem = "linux";
+{
+    enum operating_system = "linux";
+}
 else version (OSX)
-    enum operatingSystem = "macos";
+{
+    enum operating_system = "macos";
+}
 else version (Windows)
-    enum operatingSystem = "windows";
+{
+    enum operating_system = "windows";
+}
 else
-    enum operatingSystem = "unknown";
+{
+    enum operating_system = "unknown";
+}
 
 version (X86_64)
+{
     enum architecture = "x86_64";
+}
 else version (AArch64)
+{
     enum architecture = "aarch64";
+}
 else version (X86)
+{
     enum architecture = "x86";
+}
 else version (ARM)
+{
     enum architecture = "arm";
+}
 else
+{
     enum architecture = "unknown";
+}
 
-static assert(versionString == "0.1.0");
+static assert(version_string == "0.1.0");
