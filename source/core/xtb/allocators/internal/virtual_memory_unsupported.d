f05e1e9b928761c216f6ea4f4cfddcaf5427b132
Martin Ivanov <xeartybg@gmail.com>
2026-09-05 15:24:39 +0300

    style(core): migrate unsupported virtual memory backend

--- source/core/xtb/allocators/internal/virtual_memory_unsupported.d
+++ source/core/xtb/allocators/internal/virtual_memory_unsupported.d
@@ -1,58 +1,42 @@
 module xtb.allocators.internal.virtual_memory_unsupported;
 
+import xtb.types;
+
 nothrow @nogc:
 
-package(xtb.allocators.internal) enum bool virtualMemorySupported = false;
+package(xtb.allocators.internal) enum bool virtual_memory_supported = false;
 
-package(xtb.allocators.internal) alias virtual_memory_supported =
-    virtualMemorySupported;
-
-package(xtb.allocators.internal) size_t virtualMemoryPageSizeBackend() pure @safe
+package(xtb.allocators.internal) usize virtual_memory_page_size_backend() pure @safe
 {
     return 0;
 }
 
-package(xtb.allocators.internal) alias virtual_memory_page_size_backend =
-    virtualMemoryPageSizeBackend;
-
-package(xtb.allocators.internal) void* tryReserveVirtualMemoryBackend(size_t) @system
+package(xtb.allocators.internal) void* try_reserve_virtual_memory_backend(usize) pure @safe
 {
     return null;
 }
 
-package(xtb.allocators.internal) alias try_reserve_virtual_memory_backend =
-    tryReserveVirtualMemoryBackend;
-
-package(xtb.allocators.internal) bool tryCommitVirtualMemoryBackend(void*, size_t) @system
+package(xtb.allocators.internal) bool try_commit_virtual_memory_backend(void*, usize) pure @safe
 {
     return false;
 }
 
-package(xtb.allocators.internal) alias try_commit_virtual_memory_backend =
-    tryCommitVirtualMemoryBackend;
-
-package(xtb.allocators.internal) bool tryDecommitVirtualMemoryBackend(void*, size_t) @system
+package(xtb.allocators.internal) bool try_decommit_virtual_memory_backend(void*, usize) pure @safe
 {
     return false;
 }
 
-package(xtb.allocators.internal) alias try_decommit_virtual_memory_backend =
-    tryDecommitVirtualMemoryBackend;
-
-package(xtb.allocators.internal) bool releaseVirtualMemoryBackend(void*, size_t) @system
+package(xtb.allocators.internal) bool release_virtual_memory_backend(void*, usize) pure @safe
 {
     return false;
 }
-
-package(xtb.allocators.internal) alias release_virtual_memory_backend =
-    releaseVirtualMemoryBackend;
 
 unittest
 {
-    assert(!virtualMemorySupported);
-    assert(virtualMemoryPageSizeBackend() == 0);
-    assert(tryReserveVirtualMemoryBackend(4096) is null);
-    assert(!tryCommitVirtualMemoryBackend(null, 4096));
-    assert(!tryDecommitVirtualMemoryBackend(null, 4096));
-    assert(!releaseVirtualMemoryBackend(null, 4096));
+    assert(!virtual_memory_supported);
+    assert(virtual_memory_page_size_backend() == 0);
+    assert(try_reserve_virtual_memory_backend(4096) is null);
+    assert(!try_commit_virtual_memory_backend(null, 4096));
+    assert(!try_decommit_virtual_memory_backend(null, 4096));
+    assert(!release_virtual_memory_backend(null, 4096));
 }
